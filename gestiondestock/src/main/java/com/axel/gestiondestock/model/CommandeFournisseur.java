a4632d655fbf5d3552d46d89d230b14e8e530cd7
axelessono <naxelcdric@gmail.com>
2024-11-24 04:00:58 +0100

    Created Entities without adding properties

---
+++ gestiondestock/src/main/java/com/axel/gestiondestock/model/CommandeFournisseur.java
@@ -0,0 +1,17 @@
+package com.axel.gestiondestock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "commandeFournisseur")
+public class CommandeFournisseur extends AbstractEntity{
+
+
+}
---
+++ gestiondestock/src/main/java/com/axel/gestiondestock/model/MouvementStock.java
@@ -0,0 +1,15 @@
+package com.axel.gestiondestock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "mouvementStock")
+public class MouvementStock extends AbstractEntity {
+}
