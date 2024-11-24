616fc3e1fe9eb4fb7720465695a4592a4114dc1c
axelessono <naxelcdric@gmail.com>
2024-11-24 03:53:01 +0100

    Created AbstractEntity.java

---
+++ gestiondestock/src/main/java/com/axel/gestiondestock/model/AbstractEntity.java
@@ -0,0 +1,34 @@
+package com.axel.gestiondestock.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+
+    @Id
+    private Integer id;
+
+    @CreatedDate
+    @Column(name = "creationDate", nullable = false)
+    @JsonIgnore
+    private Instant creationDate;
+
+    @LastModifiedDate
+    @Column(name = "lastModifiedDate")
+    @JsonIgnore
+    private Instant lastModifiedDate;
+
+}
