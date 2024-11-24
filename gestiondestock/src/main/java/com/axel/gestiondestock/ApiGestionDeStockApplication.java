2c374ec40e0a4b99aac34366e4d3918163fa8843
axelessono <naxelcdric@gmail.com>
2024-11-24 03:52:32 +0100

    rename ApiGestionDeStockApplication.java

--- gestiondestock/src/main/java/com/axel/gestiondestock/ApiGestionDeStockApplication.java
+++ gestiondestock/src/main/java/com/axel/gestiondestock/ApiGestionDeStockApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionDeStockApplication {
+public class ApiGestionDeStockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionDeStockApplication.class, args);
+		SpringApplication.run(ApiGestionDeStockApplication.class, args);
 	}
 
 }
