53c623cc448bfa11c1443bb62b5b01a1c1ab76d4
axelessono <naxelcdric@gmail.com>
2024-11-24 03:53:01 +0100

    rename ApiGestionDeStockApplication.java

--- gestiondestock/src/test/java/com/axel/gestiondestock/ApiGestionDeStockApplicationTests.java
+++ gestiondestock/src/test/java/com/axel/gestiondestock/ApiGestionDeStockApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionDeStockApplicationTests {
+class ApiGestionDeStockApplicationTests {
 
 	@Test
 	void contextLoads() {
